19b424ac5c9adfbff6e0c8af70c012358a4f3759
El5i <118779407+El5i@users.noreply.github.com>
2023-03-06 17:46:12 +0100

    Add files via upload

---
+++ BankTest-main/TestBank/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+GENDER_CHOICES = [
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('N', 'Not Selected')
+
+]
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=50,null=True)
+    last_name = models.CharField(max_length=50,null=True)
+    email = models.EmailField()
+    number = models.FloatField(max_length=50,null=True)
+    active = models.BooleanField(default=False)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES,default='N')
+    adress = models.CharField(max_length=100,default=' ')
+    city = models.CharField(max_length=50,default=' ')
+    state = models.CharField(max_length=50,default=' ')
+    zip_code = models.CharField(max_length=10,default=' ')
+
+    
+    
+    
+    def __str__(self):
+        return self.first_name
---
+++ BankTest-main/TestBank/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import home, register_client, view_clients,user_login,dashboard
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', LoginView.as_view(template_name='login.html'), name='login'),
+    path('home/', home, name='home'),
+    path('dashboard/',dashboard, name='dashboard'),  # new URL pattern
+    path('register_client/', register_client, name='register_client'),
+    path('view_clients/', view_clients, name='view_clients'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('login/', user_login, name='user_login'),
+    
+]
+
+
+
+
+
+
